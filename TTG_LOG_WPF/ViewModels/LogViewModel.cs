c31f3dd0eef2e4e3c6b2b473481d44e9f5c79675
Dior K <qw5057@naver.com>
2022-12-01 17:03:06 +0900

    Log4net Test1

---
+++ TTG_LOG_WPF/ViewModels/LogViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using log4net;
+using TTG_LOG_WPF.Logging;
+using System.Windows.Input;
+using TTG_LOG_WPF.Models;
+
+namespace TTG_LOG_WPF.ViewModels
+{
+    public class LogViewModel : ViewModelBase
+    {
+        private ICommand _updateLogCommand;
+        public LogViewModel()
+        {
+            WriteToLog();
+        }
+
+        public void LOGINFO()
+        {
+            if (_updateLogCommand == null)
+                _updateLogCommand = new RelayCommand(() => WriteToLog());
+        }
+        private void WriteToLog()
+        {
+            Log.Write(LogLevel.Info, "HI");
+        }
+    }
+}
